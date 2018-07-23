5c3141089a44fa7a617b14485c609e94085b723e
maayankfir <mayankfir@gmail.com>
2018-07-23 20:48:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,9 +15,9 @@ def won?(board)
   win_index_3 = win_combination[2]
 
   position_1 = board[win_index_1]
-  position_2 = board[win_index_2] 
+  position_2 = board[win_index_2]
   position_3 = board[win_index_3]
-  
+
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
    return win_combination
  else
